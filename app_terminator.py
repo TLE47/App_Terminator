9c1f3c02440adf497fc80ca745e84e02e979ce18
TLE47 <91586153+TLE47@users.noreply.github.com>
2023-03-15 19:06:15 -0700

    Update app_terminator

--- app_terminator.py
+++ app_terminator.py
@@ -1,4 +1,3 @@
-# Import required libraries
 import os
 import plistlib
 import tkinter as tk
@@ -8,10 +7,9 @@ import subprocess
 import concurrent.futures
 from tqdm import tqdm
 
-# Set the allowed apps file path
 ALLOWED_APPS_FILE = "allowed_apps.txt"
+BACKGROUND_APPS_FILE = "background_apps.txt"
 
-# Load the allowed apps from the file
 def load_allowed_apps():
     if not os.path.exists(ALLOWED_APPS_FILE):
         return set()
@@ -19,13 +17,23 @@ def load_allowed_apps():
     with open(ALLOWED_APPS_FILE, "r") as f:
         return set(line.strip() for line in f.readlines())
 
-# Save the allowed apps to the file
 def save_allowed_apps(apps):
     with open(ALLOWED_APPS_FILE, "w") as f:
         for app in apps:
             f.write(app + "\n")
 
-# Get the helper apps for a give bundle path
+def load_background_apps():
+    if not os.path.exists(BACKGROUND_APPS_FILE):
+        return set()
+
+    with open(BACKGROUND_APPS_FILE, "r") as f:
+        return set(line.strip() for line in f.readlines())
+
+def save_background_apps(apps):
+    with open(BACKGROUND_APPS_FILE, "w") as f:
+        for app in apps:
+            f.write(app + "\n")
+
 def get_helper_apps(bundle_path):
     info_plist = os.path.join(bundle_path, "Contents", "Info.plist")
     if not os.path.exists(info_plist):
@@ -48,7 +56,6 @@ def get_helper_apps(bundle_path):
 
     return helpers
 
-# Terminate apps that are not in the allowed app list
 def terminate_apps(allowed_apps):
     allowed_apps_basenames = {os.path.splitext(os.path.basename(app))[0] for app in allowed_apps}
     running_processes = [proc for proc in psutil.process_iter(["name", "pid"]) if proc.info["pid"] > 0]
@@ -61,7 +68,6 @@ def terminate_apps(allowed_apps):
             except:
                 pass
 
-# Terminate a specific app by its process name
 def terminate_app(process_name):
     try:
         cmd = f'osascript -e \'quit app "{process_name}"\' >/dev/null 2>&1'
@@ -71,21 +77,18 @@ def terminate_app(process_name):
         return f"Timeout: {process_name}"
     except:
         return f"Failed to terminate: {process_name}"
-
-# Define the main App class, inheriting from tk.Tk
+    
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
-        # Set the window title and size
         self.title("App Terminator")
-        self.geometry("250x500")
+        self.geometry("500x500")
 
-        # Load the allowed apps and create the widgets
         self.allowed_apps = load_allowed_apps()
+        self.background_apps = load_background_apps()
         self.create_widgets()
 
-        # Create progress bar and status labels
         self.progress_label_var = tk.StringVar()
         self.progress_label_var.set("Progress:")
         self.progress_label = tk.Label(self, textvariable=self.progress_label_var)
@@ -101,27 +104,21 @@ class App(tk.Tk):
         self.status_label = tk.Label(self, textvariable=self.status_text)
         self.status_label.pack(pady=5)
 
-        # Create a status box to display termination status
         self.status_box = tk.Text(self, height=10, state="disabled")
         self.status_box.pack(pady=5, expand=True, fill=tk.BOTH)
 
-    # Create the UI widgets (treeview, button, etc.)
     def create_widgets(self):
-        self.tree_frame = tk.Frame(self)
-        self.tree_frame.pack(expand=True, fill=tk.BOTH)
+        self.notebook = ttk.Notebook(self)
+        self.notebook.pack(expand=True, fill=tk.BOTH)
 
-        self.tree = ttk.Treeview(self.tree_frame)
-        self.tree.heading("#0", text="Allowed Apps")
-        self.tree.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
+        self.allowed_apps_frame = tk.Frame(self.notebook)
+        self.notebook.add(self.allowed_apps_frame, text="Allowed Apps")
+        self.create_allowed_apps_section(self.allowed_apps_frame)
 
-        for app in self.allowed_apps:
-            self.tree.insert("", tk.END, text=app)
-        
-        self.scrollbar = ttk.Scrollbar(self.tree_frame, orient="vertical", command=self.tree.yview)
-        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.background_apps_frame = tk.Frame(self.notebook)
+        self.notebook.add(self.background_apps_frame, text="Background Apps")
+        self.create_background_apps_section(self.background_apps_frame)
 
-        self.tree.configure(yscrollcommand=self.scrollbar.set)
-        
         add_button = tk.Button(self, text="Add", command=self.add_app)
         add_button.pack(pady=5)
 
@@ -132,15 +129,18 @@ class App(tk.Tk):
             self, text="Terminate Apps", command=self.terminate_apps_button
         )
         terminate_button.pack(pady=5)
-        
+
         self.auto_close_var = tk.BooleanVar()
         self.auto_close_check = tk.Checkbutton(
             self, text="Auto-close", variable=self.auto_close_var
         )
         self.auto_close_check.pack(pady=5)
 
-    # Add an app to the allowed apps list
+
     def add_app(self):
+        selected_tab = self.notebook.select()
+        tab_name = self.notebook.tab(selected_tab, "text")
+
         app_path = filedialog.askopenfilename(
             title="Select App",
             filetypes=(("Application", "*.app"), ("All Files", "*.*")),
@@ -150,30 +150,47 @@ class App(tk.Tk):
             return
 
         app_name = os.path.basename(app_path)
-        self.allowed_apps.add(app_name)
-        save_allowed_apps(self.allowed_apps)
 
-        self.tree.insert("", tk.END, text=app_name)
+        if tab_name == "Allowed Apps":
+            self.allowed_apps.add(app_name)
+            save_allowed_apps(self.allowed_apps)
+            self.allowed_apps_tree.insert("", tk.END, text=app_name)
+        elif tab_name == "Background Apps":
+            self.background_apps.add(app_name)
+            save_background_apps(self.background_apps)
+            self.background_apps_tree.insert("", tk.END, text=app_name)
 
-    # Delete the selected app from the allowed apps list
     def delete_app(self):
-        selected_items = self.tree.selection()
+        selected_tab = self.notebook.select()
+        tab_name = self.notebook.tab(selected_tab, "text")
+
+        if tab_name == "Allowed Apps":
+            tree = self.allowed_apps_tree
+            app_set = self.allowed_apps
+            save_func = save_allowed_apps
+        elif tab_name == "Background Apps":
+            tree = self.background_apps_tree
+            app_set = self.background_apps
+            save_func = save_background_apps
+        else:
+            return
+
+        selected_items = tree.selection()
 
         for item in selected_items:
-            app_name = self.tree.item(item, "text")
-            self.allowed_apps.remove(app_name)
-            self.tree.delete(item)
+            app_name = tree.item(item, "text")
+            app_set.remove(app_name)
+            tree.delete(item)
 
-        save_allowed_apps(self.allowed_apps)
+        save_func(app_set)
 
-    # Terminate the apps when the terminatae button is clicked
+
     def terminate_apps_button(self):
         self.after(10, self.terminate_apps_in_gui)
 
         if self.auto_close_var.get():
             self.after(1000, self.destroy)
 
-    # Terminate the apps and update the UI accordingly
     def terminate_apps_in_gui(self):
         script_process_name = os.path.splitext(os.path.basename(__file__))[0]
         allowed_apps_basenames = {os.path.splitext(os.path.basename(app))[0] for app in self.allowed_apps}
@@ -210,9 +227,36 @@ class App(tk.Tk):
                 self.progress_var.set(i + 1)
                 self.update_idletasks()
         self.progress_label_var.set("Done!")
+    
+    def create_tree_view(self, parent, heading, app_set):
+        tree_frame = tk.Frame(parent)
+        tree_frame.pack(expand=True, fill=tk.BOTH)
+
+        tree = ttk.Treeview(tree_frame)
+        tree.heading("#0", text=heading)
+        tree.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
+
+        for app in app_set:
+            tree.insert("", tk.END, text=app)
+
+        scrollbar = ttk.Scrollbar(tree_frame, orient="vertical", command=tree.yview)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        tree.configure(yscrollcommand=scrollbar.set)
+
+        return tree
+
+
+    def create_allowed_apps_section(self, parent):
+        self.allowed_apps_tree = self.create_tree_view(parent, "Allowed Apps", self.allowed_apps)
+
+    def create_background_apps_section(self, parent):
+        self.background_apps_tree = self.create_tree_view(parent, "Background Apps", self.background_apps)
+
+    # Add remaining methods to manage background apps and their UI here.
 
 # Initialize and run the app
 if __name__ == "__main__":
     app = App()
     app.mainloop()
-    
+
